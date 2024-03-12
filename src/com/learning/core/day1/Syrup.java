29230924b7d82ea0efda5cf6f855d6c0e7561508
poorvikagn <102502801+poorvikagn@users.noreply.github.com>
2024-03-12 19:41:15 +0530

    Delete src directory

--- src/com/learning/core/day1/Syrup.java
+++
@@ -1,8 +0,0 @@
-package com.learning.core.Day1;
-
-class Syrup implements MedicineInfo {
-    @Override
-    public void displayLabel() {
-        System.out.println("Syrup is consumable only on prescription.");
-    }
-}
