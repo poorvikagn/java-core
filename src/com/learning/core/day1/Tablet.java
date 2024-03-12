0b762789c8a5e01a55701f1d0f5cf07f29a99c39
poorvikagn <102502801+poorvikagn@users.noreply.github.com>
2024-03-12 18:59:09 +0530

    Add files via upload
    day1 code added

---
+++ src/com/learning/core/day1/Tablet.java
@@ -0,0 +1,8 @@
+package com.learning.core.Day1;
+
+class Tablet implements MedicineInfo {
+    @Override
+    public void displayLabel() {
+        System.out.println("Store Tablets in a cool dry place.");
+    }
+}
